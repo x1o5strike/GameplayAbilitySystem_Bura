ce1c9511804139b74f4a32cb6ab04fad3a436b18
wonderfullgame <13651450+wonderfulllife@user.noreply.gitee.com>
2024-04-22 04:48:00 +0800

    P7. Add Hero And Goblin

--- Source/Aura/Private/Character/CharacterBase.cpp
+++ Source/Aura/Private/Character/CharacterBase.cpp
@@ -3,32 +3,15 @@
 
 #include "Character/CharacterBase.h"
 
-// Sets default values
 ACharacterBase::ACharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
+	WeaponComponent = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponComponent");
+	WeaponComponent->SetupAttachment(GetMesh(), FName("WeaponSocket"));
+	WeaponComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
-// Called when the game starts or when spawned
 void ACharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
-
-// Called every frame
-void ACharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/Aura/Public/Character/CharacterBase.h
+++ Source/Aura/Public/Character/CharacterBase.h
@@ -6,24 +6,21 @@
 #include "GameFramework/Character.h"
 #include "CharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class AURA_API ACharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ACharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category="Custom")
+	TObjectPtr<USkeletalMeshComponent> WeaponComponent;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	
 
 };
